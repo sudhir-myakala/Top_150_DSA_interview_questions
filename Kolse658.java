322def2a6380a86faf68c79c96c79c2ca19dfc10
sudhir-myakala <saisudhir1728@gmail.com>
2026-02-23 13:45:35 +0530

    added git ignore file

---
+++ Kolse658.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Kolse658 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>(Arrays.asList(1,1,2,3,4,5));
+        int k=4;
+        int  x=3;
+       
+    }
+}
--- ValidSudoko.java
+++ ValidSudoko.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+
 
 public class ValidSudoko {
     public static void main(String[] args) {
