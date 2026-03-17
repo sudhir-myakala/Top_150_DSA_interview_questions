d3f55a9990dfeb23b01a859896a33170f60a617c
sudhir-myakala <saisudhir1728@gmail.com>
2026-03-17 17:52:41 +0530

    reverseLinkedList

---
+++ ReverseLinkedList.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ReverseLinkedList {
+    public static void main(String[] args){
+       // using stack
+    //    Stack<Integer> valueStack=new Stack<>();
+
+    //     while(head!=null){
+    //         valueStack.push(head.val);
+    //         head=head.next;
+    //     }
+
+    //     ListNode rev=new ListNode(Integer.MIN_VALUE);
+    //     ListNode ptr=rev;
+
+    //     while(!valueStack.isEmpty()){
+    //         ptr.next=new ListNode(valueStack.pop());
+    //         ptr=ptr.next;
+    //     }
+
+    //     return rev.next;
+    }
+}
