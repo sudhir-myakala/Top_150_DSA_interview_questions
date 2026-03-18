0e42e7b825875a08e05046f275b7c3dc04e8ec77
sudhir-myakala <saisudhir1728@gmail.com>
2026-03-18 13:27:57 +0530

    delete from middle of the linkedList

---
+++ DeleteFromMiddleInLL.java
@@ -0,0 +1,21 @@
+public class DeleteFromMiddleInLL {
+    public static void main(String[] args){
+
+        /*
+        if(head=null || head.next==null) return null;
+
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+
+        while(fast!=null && fast.next!=null){
+        prev=slow;
+        slow=slow.next;
+        fast=fast.next.next;
+        }
+        prev.next=slow.next;
+
+        return head
+        */
+    }
+}
